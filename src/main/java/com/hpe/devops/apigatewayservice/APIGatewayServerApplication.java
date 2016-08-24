ac6c0ff46785a85192b7b9eed17cffff09543f44
Shuai Yang <shuai.yang@hpe.com>
2016-08-24 16:23:23 +0800

    修改配置错误

---
+++ src/main/java/com/hpe/devops/apigatewayservice/APIGatewayServerApplication.java
@@ -0,0 +1,17 @@
+package com.hpe.devops.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class APIGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APIGatewayServerApplication.class, args);
+	}
+
+}
